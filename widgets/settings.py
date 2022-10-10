0d135ea6e27244d1694dd9ae4ff04b7823820387
pychthonic <fridgewolf@protonmail.com>
2022-10-10 17:52:57 -0400

    working on permissions for swagger

--- widgets/settings.py
+++ widgets/settings.py
@@ -108,6 +108,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
